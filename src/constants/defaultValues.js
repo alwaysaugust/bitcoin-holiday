bb1dec4245b190e7917f487d5a37eaac3f5da324
David Jin <94069932+davidjin-ca@users.noreply.github.com>
2021-11-23 21:50:30 -0500

    remove breadcrumb

--- src/constants/defaultValues.js
+++ src/constants/defaultValues.js
@@ -1,28 +1,25 @@
 export const defaultLocale = 'en';
 export const localeOptions = [
   { id: 'en', name: 'English - LTR', direction: 'ltr' },
-  { id: 'es', name: 'Español', direction: 'ltr' },
-  { id: 'enrtl', name: 'English - RTL', direction: 'rtl' },
 ];
-
-export const adminRoot = '/app';
-export const servicePath = 'http://localhost:3001';
 
 export const themeColorStorageKey = '__theme_selected_color';
 export const defaultColor = 'light';
 export const defaultDirection = 'ltr';
-export const isAuthGuardActive = false;
 
 export const API_KEY = 'AIzaSyDOoEpcGx2-v76hZvaeWjXy-i8WEoBe9e4';
+export const CLIENT_ID =
+  '897127869706-03r5sarg94aciafgr4p4sgs32q0v0t3n.apps.googleusercontent.com';
+
 export const DISCOVERY_DOCS = [
   'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest',
 ];
-export const CLIENT_ID =
-  '897127869706-03r5sarg94aciafgr4p4sgs32q0v0t3n.apps.googleusercontent.com';
+
 export const SCOPES =
   'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar.events';
 export const defaultImgLink =
   'https://ciphertrace.com/wp-content/uploads/2018/11/criminal-bitcoin-how-deep-is-the-dark-side-of-the-most-widely-traded-cryptocurrency.jpg';
+
 export const months = [
   {
     name: 'January',
